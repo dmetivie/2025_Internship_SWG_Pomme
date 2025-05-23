d71297aba2da85fe3279a582428d5f3e5163baa0
ArnaudG0649 <arnaudcmc@hotmail.com>
2025-05-23 16:58:07 +0200

    Phenology simple plots

--- Phenology/P_Estimation.jl
+++ Phenology/P_Estimation.jl
@@ -50,3 +50,42 @@ function Phenology_Estimation(temp::AbstracTemperature;
                                 forcing_model=forcing_model,
                                 forcing_target=forcing_target)
 end
+
+
+
+
+ScaleDate(date_::Date, JED=(10, 1)) = (dayofyear_Leap(date_) - dayofyear_Leap(Date(0, JED[1], JED[2]) - Day(1)) + 366) .% 366
+ScaleDate(nt2::Real, JED=(10, 1)) = (nt2 - dayofyear_Leap(Date(0, JED[1], JED[2]) - Day(1)) + 366) .% 366
+
+function Plot_Pheno_Dates(date_vec, title)
+
+    Ref_date_vec = ScaleDate.(date_vec)
+
+    MiddleMonthIndex = (cumsum(DaysPerMonth(0)) .+ cumsum([0; DaysPerMonth(0)])[1:12]) ./ 2
+    CurrentMonths = unique(month.(date_vec))
+
+    y_min = ScaleDate(Date(0, month(date_vec[argmin(Ref_date_vec)])))
+    y_max = ScaleDate(Date(0, month(date_vec[argmax(Ref_date_vec)] + Month(1))) - Day(1))
+
+    fig = Figure()
+    
+    ax2 = Axis(fig[1:2, 1:2], yticks=(ScaleDate.(MiddleMonthIndex[CurrentMonths]), Month_vec[CurrentMonths]))
+    ax2.limits = (nothing, [y_min, y_max])
+    ax2.yticksvisible = false
+    ax2.ygridvisible = false
+    ax2.xticksvisible = false
+    ax2.xgridvisible = false
+    ax2.xticklabelsvisible= false
+
+    ax = Axis(fig[1:2, 1:2])
+    ax.yticks = [ScaleDate.(cumsum([0; DaysPerMonth(0)])[CurrentMonths] .+ 1); y_max]
+    ax.yticklabelsvisible = false
+    ax.limits = (nothing, [y_min, y_max])
+    ax.xlabel = "Year"
+    ax.ylabel = "Date"
+    ax.title = "$(title) dates for each year"
+
+    plt = lines!(ax, year.(date_vec), Ref_date_vec)
+
+    return fig
+end
